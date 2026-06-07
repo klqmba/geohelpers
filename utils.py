c9b7ed39c0a559010873ef72e2b5a58bfceeefda
klqmba <92926033+klqmba@users.noreply.github.com>
2026-06-07 20:12:42 +0200

    added f3 and f4 DF filter functions

--- utils.py
+++ utils.py
@@ -1,6 +1,41 @@
 import pandas as pd
 from rasterstats import zonal_stats
 from pyproj import CRS
+import numbers
+
+def f3(df,d): 
+    '''
+    Generic function to filter a dataframe by column values passing a dict d with the required columns as keys,
+    and values that might be lists or single values of string or numbers.
+    '''
+    aux = []
+    for k,v in d.items():
+        if isinstance(v, numbers.Number):
+            aux.append(f'`{k}`=={v}')
+        elif isinstance(v, list):    
+            aux.append(f'`{k}` in {v}')
+        else:
+            aux.append(f'`{k}`=="{v}"')
+    ref = ' and '.join(aux)
+#     print(ref)
+    return df.query(ref)
+    
+def f4(df,d): 
+    '''
+    Generic function to filter a dataframe by column values passing a dict d with the required columns as keys,
+    and values that might be lists or single values of string or numbers.
+    '''
+    aux = []
+    for k,v in d.items():
+        if isinstance(v, numbers.Number):
+            aux.append(f'`{k}`!={v}')
+        elif isinstance(v, list):    
+            aux.append(f'`{k}` not in {v}')
+        else:
+            aux.append(f'`{k}`!="{v}"')
+    ref = ' and '.join(aux)
+#     print(ref)
+    return df.query(ref)
 
 def get_grid_info(df,raster_path, agg='sum', col='VOL', all_touched=False):
 	df_orig_crs = df.crs
